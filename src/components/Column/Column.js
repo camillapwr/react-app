a245f773fe4b4aa48b38744e767f2fef8afe92bf
Kamila Gajcy <kamila.gajcy@gmail.com>
2021-07-12 14:37:24 +0200

    Create ListContainer to display columns

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
-import Creator from '../Creator/Creator.js';
-import Card from '../Card/Card.js';
+//import Creator from '../Creator/Creator.js';
+//import Card from '../Card/Card.js';
 import { settings } from '../../data/dataStore';
 import Icon from './Icon';
-
  
 class Column extends React.Component {
-    state = {
-      cards: this.props.cards || [],
-    }
+    
     static propTypes = {
       title: PropTypes.string,
       //key: PropTypes.number,
@@ -21,28 +18,16 @@ class Column extends React.Component {
       icon: settings.defaultColumnIcon,
     }
 
-    addCard(title){
-      this.setState(state => (
-        {
-          cards: [
-            ...state.cards,
-            {
-              key: state.cards.length ? state.cards[state.cards.length-1].key+1 : 0,
-              title,
-            },
-          ],
-        }
-      ));
-    }
-
     render() {
+      const {title, icon} = this.props;
       return (
         <section className={styles.component}>
-          <h3 className={styles.title}>{this.props.title}
+          <h3 className={styles.title}>{title}
             <span className={styles.icon}>
-              <Icon name={this.props.icon} />
+              <Icon name={icon} />
             </span>
           </h3>
+          {/*
           <div className={styles.cards}>
             {this.state.cards.map(({key, ...cardProps}) => (
               <Card key={key} {...cardProps} />
@@ -51,6 +36,7 @@ class Column extends React.Component {
           <div className={styles.creator}>
             <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
           </div>
+            */}
         </section>
       );
     }
---
+++ src/components/List/ListContainer.js
@@ -0,0 +1,14 @@
+import {connect} from 'react-redux';
+import List from './List';
+
+export const getColumnsForList = ({columns}, listId) => columns.filter(column => column.listId == listId);
+
+{/*const mapStateToProps = state => ({
+  columns: state.columns,
+
+});*/}
+const mapStateToProps = (state, props) => ({
+  columns: getColumnsForList(state, props.id),
+});
+
+export default connect(mapStateToProps)(List);
