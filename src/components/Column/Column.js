3246195dc2d6371721295c10381963b10b535d96
Kamila Gajcy <kamila.gajcy@gmail.com>
2021-07-12 19:39:14 +0200

    12.2 Create ColumnContainer to display cards in columns

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 //import Creator from '../Creator/Creator.js';
-//import Card from '../Card/Card.js';
+import Card from '../Card/Card.js';
 import { settings } from '../../data/dataStore';
 import Icon from './Icon';
  
@@ -19,7 +19,7 @@ class Column extends React.Component {
     }
 
     render() {
-      const {title, icon} = this.props;
+      const {title, icon, cards} = this.props;
       return (
         <section className={styles.component}>
           <h3 className={styles.title}>{title}
@@ -27,12 +27,13 @@ class Column extends React.Component {
               <Icon name={icon} />
             </span>
           </h3>
-          {/*
+          
           <div className={styles.cards}>
-            {this.state.cards.map(({key, ...cardProps}) => (
-              <Card key={key} {...cardProps} />
+            {cards.map(cardData => (
+              <Card key={cardData.id} {...cardData} />
             ))}
           </div>
+          {/*
           <div className={styles.creator}>
             <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
           </div>
